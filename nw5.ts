46c55dc646f509803c098bb854457644c5eb6f63
bramho <bramhoningh16@gmail.com>
2016-11-30 10:46:49 +0100

    Added challenges

---
+++ nw5.ts
@@ -0,0 +1,37 @@
+// 2.    Maak 6 string variables met elk de naam van een teamlid
+
+// 3.    Schrijf iedere naam naar de ID "typescript-output"
+
+// 4.    Maak 6 integer variables met de leeftijden van ieder teamlid
+
+// 5.    Schrijf iedere naam + de leeftijd naar "typescript-output" als "[naam] is [leeftijd] jaar oud"
+
+// 6.    Maak een namen array met de namen erin van ieder teamlid
+
+// 7.    Maak een for-loop die door de namen loopt en alle namen naar "typescript-output" uitpoept
+
+// 8.    Maak een interface Teamleden met een string voornaam, string achternaam en een integer leeftijd
+
+// 9.    Maak 6 instance variables van de Teamleden interface en zet hierin de voornaam, achternaam en leeftijd
+
+// 10.   Schrijf de namen en leeftijden naar "typescript-output"
+
+// 11.   Maak een class Persoon aan en maak hierin een constructor aan die de voornaam, achternaam, leeftijd en een boolean isTeamlid opmeent
+
+// 12.   Maak een functie aan in de class Persoon die de voornaam, achternaam, leeftijd en isTeamlid naar "typescript-output" schrijft
+
+// 12.   Maak 2 variables aan die een nieuwe instance zijn van de class Persoon, geef hierin de namen, leeftijd aan en maak er eentje een teamlid en  de andere niet
+
+// 13.   Roep voor beide variables de functie aan die de voornaam, achternaam, leeftijd en isTeamlid naar "typescript-output" schrijft
+
+// 14.   Maak een functie aan in Persoon die met een if-statement kijkt of een persoon een teamlid is en schrijft die naar "typescript-output"
+
+// 15.   Roep voor beide varaibles de functie met de if-statement aan
+
+// 16.   Maak een class aan van jezelf die de class Persoon extends met een constructor die de voornaam, achternaam, leeftijd en isTeamlid opneemt
+
+// 17.   Stuur deze variables door naar de Persoon class
+
+// 18.   Check of jij een teamlid bent
+
+// 19.   Schrijf jou gegevens naar "typescript-output"
